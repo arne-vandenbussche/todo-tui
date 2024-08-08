d72b9e96fa09ceee8d2f2fab94a5d18034ce7b1a
Arne Vandenbussche <arne.vandenbussche@gmail.com>
2024-08-08 19:37:25 +0200

    by pressing keys we can switch between different lists (sorted and/or filtered

--- todo.py
+++ todo.py
@@ -3,18 +3,46 @@ from database import TaskDatabase
 import npyscreen
 
 class RecordList(npyscreen.MultiLineAction):
-    pass
+    def __init__(self, *args, **keywords):
+        super(RecordList, self).__init__(*args, **keywords)
+        self.add_handlers({
+                        "a": self.set_list_all_tasks, 
+                        "d": self.set_list_todo_by_deadline,
+                        "c": self.set_list_todo_by_planned
+        })
+
+    def set_list_todo_by_deadline(self, *args, **keywords):
+        all_tasks = self.parent.parentApp.myDatabase.get_all_tasks()
+        todo_tasks = [task for task in all_tasks if task.status == 'todo']
+        sorted_todo_tasks_by_deadline = sorted(todo_tasks, key=lambda task: task.date_deadline, reverse=True)
+        self.parent.display_list = sorted_todo_tasks_by_deadline
+        self.parent.update_list()
+
+    def set_list_all_tasks(self, *args, **keywords):
+        all_tasks = self.parent.parentApp.myDatabase.get_all_tasks()
+        sorted_tasks = sorted(all_tasks, key=lambda task: task.id, reverse=True)
+        self.parent.display_list = sorted_tasks
+        self.parent.update_list()
+
+    def set_list_todo_by_planned(self, *args, **keywords):
+        all_tasks = self.parent.parentApp.myDatabase.get_all_tasks()
+        todo_tasks = [task for task in all_tasks if task.status == 'todo']
+        sorted_todo_tasks_by_planned = sorted(todo_tasks, key=lambda task: task.date_planned, reverse=True)
+        self.parent.display_list = sorted_todo_tasks_by_planned
+        self.parent.update_list()
+
 
 class RecordListDisplay(npyscreen.FormMutt):
     MAIN_WIDGET_CLASS = RecordList
     
     def beforeEditing(self):
+        self.display_list = self.parentApp.myDatabase.get_all_tasks()
         self.update_list()
 
     def update_list(self):
-        self.wStatus1.value = "a: all tasks | d: todo by deadline | p: todo by planned | t: today | D: done | C: cancelled"
+        self.wStatus1.value = "a: all tasks | d: todo by deadline | c: todo by planned | t: today | D: done | C: cancelled"
         self.wStatus2.value = "ctrl-a: add | ctrl-d: delete | ctrl-x: set done | ctrl-c: cancel | enter: update task | :q = quit"
-        self.wMain.values = self.parentApp.myDatabase.get_all_tasks()
+        self.wMain.values = self.display_list
         self.wMain.display()
 
 
