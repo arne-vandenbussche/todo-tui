8ef7c5b109b3ce6239e010e8d110ec03d6e67ddf
Arne Vandenbussche <arne.vandenbussche@gmail.com>
2024-08-09 19:01:26 +0200

    add, remove and update task; set task done; set taks canceled; quit application; search for task

--- database.py
+++ database.py
@@ -1,5 +1,8 @@
 import sqlite3
 from task import Task
+import sys
+import logging
+logger = logging.getLogger(__name__)
 
 class TaskDatabase:
     def __init__(self, filename="todo.sqlite3"):
@@ -22,7 +25,13 @@ class TaskDatabase:
         my_cursor.execute(sql_create_statement)
         db_connection.commit()
         db_connection.close()
-
+    
+    def convert_tuple_to_task(self, task_tuple: tuple) -> Task:
+        my_task = Task(task_tuple[0], task_tuple[1], task_tuple[2], task_tuple[3],
+                       task_tuple[4], task_tuple[5], task_tuple[6], task_tuple[7], 
+                       task_tuple[8])
+        return my_task
+    
     def create_task(self, my_task: Task):
         sql_insert_statement = f"""INSERT INTO tasks 
                                         (description, date_deadline, date_planned, 
@@ -38,8 +47,7 @@ class TaskDatabase:
                 my_cursor.execute(sql_insert_statement, task)
                 my_connection.commit()
         except sqlite3.Error as e:
-            print("Error while inserting into database: ")
-            print(e)
+            logger.error(f"Error while inserting into database: {repr(e)}")
 
 
     def delete_task(self, task_id: int):
@@ -50,34 +58,7 @@ class TaskDatabase:
                 my_cursor.execute(sql_delete_statement, (task_id,))
                 my_connection.commit()
         except sqlite3.Error as e:
-            print("Eror while deleting task from database:")
-            print(e)
-
-    def update_task(self, my_task: Task):
-        sql_update_statement = """UPDATE tasks
-        SET description = ?, date_deadline=?, 
-        date_planned=?, date_done=?, date_canceled=?,
-        status=?, tags=?,refs=?
-        WHERE id=?
-        """
-        update_task_tuple=(my_task.description, my_task.date_deadline,
-                           my_task.date_planned, my_task.date_done,
-                           my_task.date_canceled, my_task.status, 
-                           my_task.tags, my_task.refs, my_task.id)
-        try:
-            with sqlite3.connect(self.db_filename) as my_connection:
-                my_cursor = my_connection.cursor()
-                my_cursor.execute(sql_update_statement, update_task_tuple)
-                my_connection.commit()
-        except sqlite3.Error as e:
-            print("Error while updating task: ")
-            print(e)
-
-    def convert_tuple_to_task(self, task_tuple: tuple) -> Task:
-        my_task = Task(task_tuple[0], task_tuple[1], task_tuple[2], task_tuple[3],
-                       task_tuple[4], task_tuple[5], task_tuple[6], task_tuple[7], 
-                       task_tuple[8])
-        return my_task
+            logger.error(f"Error while deleting task from database: {repr(e)}")
 
     def get_all_tasks(self) -> list[Task]:
         sql_select_all_statement="SELECT * FROM tasks;"
@@ -88,10 +69,59 @@ class TaskDatabase:
                 my_cursor.execute(sql_select_all_statement)
                 rows = my_cursor.fetchall()
         except sqlite3.Error as e:
-            print("Error while retrieving all tasks from database: ", e)
+            logger.error(f"Error while retrieving all tasks from database: {repr(e)}")
         # rows is a list of tuples, we will convert this to a row of Task objects
         task_list = [self.convert_tuple_to_task(task_tuple) for task_tuple in rows]
         return task_list
+
+    def get_one_task(self, task_id: int) -> Task:
+        sql_one_task="SELECT * FROM tasks WHERE id=?;"
+        try:
+            with sqlite3.connect(self.db_filename) as my_connection:
+                my_cursor = my_connection.cursor()
+                my_cursor.execute(sql_one_task, (int(task_id),))
+                task_tupel = my_cursor.fetchone()
+        except sqlite3.Error as e:
+            logger.critical(f"Error while retrieving one task from database: {repr(e)}")
+            sys.exit(1) 
+        if task_tupel is not None:
+            task = self.convert_tuple_to_task(task_tupel)
+            return task
+        else:
+            logger.error(f"Task with {task_id} does not exist")
+            sys.exit(1)
+
+    def update_task(self, task: Task):
+        sql_update_task="""UPDATE tasks
+        SET description = ?, date_deadline = ?, date_planned = ?,
+        date_done = ?, date_canceled = ?,
+        status = ?, tags = ?, refs = ?
+        WHERE id=?;
+        """
+        try:
+            with sqlite3.connect(self.db_filename) as my_connection:
+                my_cursor = my_connection.cursor()
+                my_cursor.execute(sql_update_task,(task.description, str(task.date_deadline),
+                    str(task.date_planned), str(task.date_done), str(task.date_canceled),
+                    str(task.status), str(task.tags), str(task.refs), str(task.id),))
+        except sqlite3.Error as e:
+            logger.error(f"Error while updating task {task.id}: {repr(e)}")
+
+    def add_task(self, task: Task):
+        sql_add_task="""INSERT into tasks (description, date_deadline,
+                            date_planned, date_done, date_canceled,
+                            status, tags, refs) VALUES
+                            (?, ?, ?, ?, ?, ?, ?, ?);
+        """
+        try:
+            with sqlite3.connect(self.db_filename) as my_connection:
+                my_cursor = my_connection.cursor()
+                my_cursor.execute(sql_add_task, (task.description, str(task.date_deadline),
+                                  str(task.date_planned), str(task.date_done), 
+                                  str(task.date_canceled), str(task.status), str(task.tags), str(task.refs),)
+                                  )
+        except sqlite3.Error as e:
+            logger.error(f"Error while creating task: {repr(e)}")
 
 
 if __name__ == '__main__':
--- todo.py
+++ todo.py
@@ -2,6 +2,8 @@ from task import Task
 from database import TaskDatabase
 import npyscreen
 from datetime import datetime
+import logging
+logger = logging.getLogger(__name__)
 
 class RecordList(npyscreen.MultiLineAction):
     def __init__(self, *args, **keywords):
@@ -12,8 +14,44 @@ class RecordList(npyscreen.MultiLineAction):
                         "c": self.set_list_todo_by_planned,
                         "D": self.set_list_done,
                         "C": self.set_list_canceled,
-                        "t": self.set_list_today
+                        "t": self.set_list_today,
+                        "^D": self.when_delete_record,
+                        "^A": self.when_add_record,
+                        "^X": self.when_set_task_done,
+                        "^Q": self.when_cancel_task
         })
+
+    def actionHighlighted(self, act_on_this, keypress):
+        self.parent.parentApp.getForm('EDITRECORDFM').value=act_on_this.id
+        self.parent.parentApp.switchForm('EDITRECORDFM')
+
+    def when_delete_record(self, *args, **keywords):
+        self.parent.parentApp.myDatabase.delete_task(self.values[self.cursor_line].id)
+        all_tasks = self.parent.parentApp.myDatabase.get_all_tasks()
+        self.parent.display_list = all_tasks
+        self.parent.update_list()
+
+    def when_add_record(self, *args, **keywords):
+        self.parent.parentApp.getForm('EDITRECORDFM').value = None
+        self.parent.parentApp.switchForm('EDITRECORDFM')
+
+    def when_set_task_done(self, *args, **keywords):
+        task = self.values[self.cursor_line]
+        task.status = 'done'
+        task.date_done = datetime.today().strftime('%Y-%m-%d')
+        self.parent.parentApp.myDatabase.update_task(task)
+        all_tasks = self.parent.parentApp.myDatabase.get_all_tasks()
+        self.parent.display_list = all_tasks
+        self.parent.update_list()
+
+    def when_cancel_task(self, *args, **keywords):
+        task = self.values[self.cursor_line]
+        task.status = 'canceled'
+        task.date_canceled = datetime.today().strftime('%Y-%m-%d')
+        self.parent.parentApp.myDatabase.update_task(task)
+        all_tasks = self.parent.parentApp.myDatabase.get_all_tasks()
+        self.parent.display_list = all_tasks
+        self.parent.update_list()
 
     def set_list_todo_by_deadline(self, *args, **keywords):
         all_tasks = self.parent.parentApp.myDatabase.get_all_tasks()
@@ -57,29 +95,109 @@ class RecordList(npyscreen.MultiLineAction):
         self.parent.display_list = today_tasks
         self.parent.update_list()
 
-class RecordListDisplay(npyscreen.FormMutt):
+class MyActionController(npyscreen.ActionControllerSimple):
+    def create(self):
+        self.add_action('^quit', self.quit_application, False)
+        self.add_action('^/.*', self.set_search, False)
+
+    def set_search(self, command_line, control_widget_proxy, live):
+        search_term = command_line[1:].strip()
+        filtered_list = [task for task in self.parent.display_list if search_term.lower() in task.description.lower() or search_term in task.tags.lower()]
+        self.parent.display_list = filtered_list
+        self.parent.update_list()
+
+    def quit_application(self, command_line, control_widget_proxy, live):
+        self.parent.parentApp.switchForm(None)
+
+class RecordListDisplay(npyscreen.FormMuttActive):
     MAIN_WIDGET_CLASS = RecordList
-    
+    ACTION_CONTROLLER = MyActionController
+
     def beforeEditing(self):
         self.display_list = self.parentApp.myDatabase.get_all_tasks()
         self.update_list()
 
     def update_list(self):
-        self.wStatus1.value = "a: all tasks | d: todo by deadline | c: todo by planned | t: today | D: done | C: cancelled"
-        self.wStatus2.value = "ctrl-a: add | ctrl-d: delete | ctrl-x: set done | ctrl-c: cancel | enter: update task | :q = quit"
+        self.wStatus1.value = "VIEWS: a: all tasks | d: todo by deadline | c: todo by planned | t: today | D: done | C: cancelled"
+        self.wStatus2.value = "ACTIONS: ctrl-a: add | ctrl-d: delete | ctrl-x: set done | ctrl-q: cancel | enter: update task | command line + quit: quit | command line + /: search" 
         self.wMain.values = self.display_list
         self.wMain.display()
 
 
 class EditRecord(npyscreen.ActionForm):
-    pass
+    def create(self):
+        self.value = None
+        self.wgDescription = self.add(npyscreen.TitleText, name = "Description: ")
+        self.wgDate_deadline = self.add(npyscreen.TitleText, name = "Deadline: ")
+        self.wgDate_planned = self.add(npyscreen.TitleText, name = "Planned date: ")
+        self.wgTags = self.add(npyscreen.TitleText, name = "Tags: ")
+        self.wgRefs = self.add(npyscreen.TitleText, name = "Refs: ")
+        
+        self.wgStatus = self.add(npyscreen.TitleFixedText, name = "Status: ")
+        self.wgDate_done = self.add(npyscreen.TitleFixedText, name = "Date done: ")
+        self.wgDate_canceled = self.add(npyscreen.TitleFixedText, name = "Date canceled: ")
+        self.wgInfo = self.add(npyscreen.FixedText)
+        self.wgInfo.value = "ctrl-s: save and go back to list | ctrl-q: go back without saving"
+        self.add_handlers({"^S": self.save_task_and_go_back,
+                           "^Q": self.go_back}) 
+
+
+    def beforeEditing(self):
+        if self.value:
+            task = self.parentApp.myDatabase.get_one_task(self.value)
+            self.name = f"Task id = {str(task.id)}"
+            self.task_id = task.id
+            self.wgDescription.value = str(task.description)
+            self.wgDate_deadline.value = str(task.date_deadline)
+            self.wgDate_planned.value = str(task.date_planned)
+            self.wgTags.value = str(task.tags)
+            self.wgRefs.value = str(task.refs)
+            self.wgStatus.value = str(task.status)
+            self.wgDate_done.value = str(task.date_done)
+            self.wgDate_canceled.value = str(task.date_canceled)
+        else:
+            self.name = "New task"
+            self.task_id = ''
+            self.wgDescription.value = ''
+            self.wgDate_deadline.value = ''
+            self.wgDate_planned.value = ''
+            self.wgTags.value = ''
+            self.wgRefs.value = ''
+            self.wgStatus.value = ''
+            self.wgDate_done.value = ''
+            self.wgDate_canceled.value = ''
+
+    def on_ok(self):
+        self.save_task_and_go_back(event=None)
+
+    def on_cancel(self):
+        self.parentApp.switchFormPrevious()
+
+    def save_task_and_go_back(self, event):
+        if self.task_id: # we are editing an existing task
+            edited_task = Task(id=int(self.task_id), description=self.wgDescription.value, 
+                               date_deadline=self.wgDate_deadline.value, date_planned=self.wgDate_planned.value,
+                               date_done=self.wgDate_done.value, date_canceled=self.wgDate_canceled.value,
+                               status=self.wgStatus.value, tags=self.wgTags.value, refs=self.wgRefs.value)
+            self.parentApp.myDatabase.update_task(edited_task)
+        else:
+            added_task = Task(id=None, description=self.wgDescription.value, 
+                               date_deadline=self.wgDate_deadline.value, date_planned=self.wgDate_planned.value,
+                               date_done='', date_canceled='', status='todo', tags=self.wgTags.value, refs=self.wgRefs)
+            self.parentApp.myDatabase.add_task(added_task)
+        self.parentApp.switchFormPrevious()
+
+    def go_back(self, event):
+        self.parentApp.switchFormPrevious()
 
 class TaskApplication(npyscreen.NPSAppManaged):
     def onStart(self):
         self.myDatabase = TaskDatabase()
         self.addForm("MAIN", RecordListDisplay)
-        self.addForm("EDITRECDORDFM", EditRecord)
+        self.addForm("EDITRECORDFM", EditRecord)
 
 if __name__ == '__main__':
+    # first configure logger
+    logging.basicConfig(filename='todo.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p')
     myApp = TaskApplication()
     myApp.run()
